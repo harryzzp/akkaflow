0da87f25a93c202fbaa2a959b3900b3664c831d0
harryzzp <3123822>
2016-03-10 22:22:10 +0800

    add files

---
+++ src/main/java/de/kimrudolph/akkaflow/controller/DemoController.java
@@ -0,0 +1,19 @@
+package de.kimrudolph.akkaflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Skywalker on 2016/3/10.
+ */
+@Controller
+public class DemoController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/main/java/de/kimrudolph/akkaflow/rest/HelloController.java
@@ -0,0 +1,20 @@
+package de.kimrudolph.akkaflow.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/say")
+    public String say(@RequestParam(value = "name", required = false, defaultValue = "Jedi") String name) {
+        return name + ", greeting from Spring Boot!";
+    }
+    
+}
