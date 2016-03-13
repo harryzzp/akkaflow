4b89122866b95677a155c69c96b61f2a3f279028
sparksfly <harryzzp@gmail.com>
2016-03-13 14:12:24 +0800

    add mvc

---
+++ src/main/java/de/kimrudolph/akkaflow/controller/SampleController.java
@@ -0,0 +1,23 @@
+package de.kimrudolph.akkaflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Skywalker on 2016/3/13.
+ */
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+    @RequestMapping("index.html")
+    public String index(Model model,
+                        @RequestParam(value = "company", defaultValue = "国联证券") String company) {
+        model.addAttribute("company", company);
+        return "sample/index";
+    }
+
+
+}
